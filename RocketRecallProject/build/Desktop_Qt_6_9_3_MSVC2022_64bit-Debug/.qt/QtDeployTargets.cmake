2c69e1f27983ae9c3f2a2ae2642c2ea5ee899948
Jakefeldmanstarosta <22hfry@queensu.ca>
2025-11-22 13:35:47 -0500

    cleanup: remove build + Qt cache from repo

--- RocketRecallProject/build/Desktop_Qt_6_9_3_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_RocketRecallProject_FILE C:/Users/jakef/rocket-recall/RocketRecallProject/build/Desktop_Qt_6_9_3_MSVC2022_64bit-Debug/RocketRecallProject.exe)
-set(__QT_DEPLOY_TARGET_RocketRecallProject_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_RocketRecallProject_RUNTIME_DLLS C:/Qt/6.9.3/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.9.3/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.9.3/msvc2022_64/bin/Qt6Cored.dll)
